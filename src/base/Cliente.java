727b022278f89ba0247e9ca05c5946d3109d7e62
Huawei <Huawei@LAPTOP-IA9K0L50>
2023-12-06 09:48:24 -0600

    Constructor + Getter + Setter

--- src/base/Cliente.java
+++ src/base/Cliente.java
@@ -11,4 +11,37 @@ package base;
 public class Cliente {
     private int cedula;
     private String nombre;
+    private String direccion;
+
+    public Cliente(int cedula, String nombre, String direccion) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
 }
