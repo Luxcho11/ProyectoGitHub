74a92e8bf1153d9b52e5a48d7850d7f8c9295db3
Huawei <Huawei@LAPTOP-IA9K0L50>
2023-12-06 09:31:26 -0600

    Carga inicial

---
+++ src/base/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package base;
+
+/**
+ *
+ * @author Huawei
+ */
+public class Cliente {
+    private int cedula;
+    private String nombre;
+}
